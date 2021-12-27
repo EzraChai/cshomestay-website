e0c0b3385dd4d5262d797afb25b4efe5dcd96edf
EzraChai <juanzhe2@gmail.com>
2021-12-28 01:48:36 +0800

    third day

--- components/PhoneGallery.jsx
+++ components/PhoneGallery.jsx
@@ -5,11 +5,11 @@ export const PhoneGallery = () => {
     return (
         <div className="px-6 mt-4">
             {photos.map((picture, i) => (
-                <div key={i} className="bg-zinc-100 rounded-sm mb-6 overflow-hidden pb-6 p-3 shadow-lg">
+                <div key={i} className=" bg-slate-100 rounded-sm mb-6 overflow-hidden pb-6 p-3 shadow-lg">
                     <div className="">
                         <Image src={picture.src} alt={picture.title}/>
                     </div>
-                    <div className="title mt-1 cursive font-semibold ml-1 text-xl">
+                    <div className="title mt-1 cursive font-semibold ml-2 tracking-wider text-xl">
                         {picture.title}
                     </div>
                 </div>
