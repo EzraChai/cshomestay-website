4f2a33342a3177802f2f69c2c77765d2719c7ecd
EzraChai <juanzhe2@gmail.com>
2021-12-28 01:27:37 +0800

    third day

--- components/PhoneGallery.jsx
+++ components/PhoneGallery.jsx
@@ -3,10 +3,10 @@ import { photos } from "./photos"
 
 export const PhoneGallery = () => {
     return (
-        <div className="px-6 snap-mandatory snap-y">
+        <div className="px-6">
             {photos.map((picture, i) => (
-                <div key={i} className="bg-zinc-100 snap-center mb-6 rounded-lg overflow-hidden pb-6 p-2 shadow-lg">.
-                    <div className="touch-pinch-zoom">
+                <div key={i} className="bg-zinc-100 mb-6 rounded-lg overflow-hidden pb-6 p-3 shadow-lg">
+                    <div className="">
                         <Image src={picture.src} alt={picture.title}/>
                     </div>
                     <div className="title">
