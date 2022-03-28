ed9d4d84d3857e271f70e572f98bbb408ccdb5a1
EzraChai <juanzhe2@gmail.com>
2022-03-28 23:22:01 +0800

    fixed bug

--- components/StrategicPlace.jsx
+++ components/StrategicPlace.jsx
@@ -72,7 +72,7 @@ export const StrategicPlace = () => {
                 </div>
                 <div className="col-span-2 overflow-hidden">
                   <div className="content-right flex justify-center py-10">
-                    <iframe className="border-0 mt-6 aspect-video lg:overflow-hidden mx-6 lg:rounded-lg lg:w-full" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1993.048515417508!2d101.85464963425447!3d2.474808281170744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cdf77fbf9bcc17%3A0x573234c80a0083d7!2sCozy%20%26%20Sweet%20Homestay!5e0!3m2!1sen!2smy!4v1640871664571!5m2!1sen!2smy" allowFullScreen loading="lazy"></iframe>
+                    {/* <iframe className="border-0 mt-6 aspect-video lg:overflow-hidden mx-6 lg:rounded-lg lg:w-full" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1993.048515417508!2d101.85464963425447!3d2.474808281170744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cdf77fbf9bcc17%3A0x573234c80a0083d7!2sCozy%20%26%20Sweet%20Homestay!5e0!3m2!1sen!2smy!4v1640871664571!5m2!1sen!2smy" allowFullScreen loading="lazy"></iframe> */}
                   </div>
                 </div>
               </div>
