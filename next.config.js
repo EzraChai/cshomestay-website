7171996f605182bda4b1562ad7caa7d041fbcaed
Ezra Chai <juanzhe2@gmail.com>
2022-11-26 21:35:04 +0800

    refractor

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // swcMinify: true,
+  swcMinify: true,
   reactStrictMode: true,
-   images: {
-    domains: ['media.graphcms.com'],
+  images: {
+    domains: ["media.graphassets.com"],
   },
-}
+};
