d352f86ed387ff8c6b6c14e3a8963578567eaf9c
Ezra Chai <juanzhe2@gmail.com>
2022-12-21 11:06:51 +0800

    fixed responsive 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className=" flex min-h-screen justify-center items-center">
+    <div className="absolute inset-0 flex h-full justify-center items-center">
       <div className="absolute flex items-center justify-center text-[14rem] md:text-[24rem] font-extrabold text-zinc-50 z-[-1] inset-0">
         404
       </div>
       <div className="flex justify-center items-center flex-col">
-        <div className="text-[7rem] bg-gradient-to-r from-red-600 bg-clip-text text-transparent to-orange-400 font-extrabold uppercase">
+        <div className=" text-[6rem] lg:text-[7rem] bg-gradient-to-r from-red-600 bg-clip-text text-transparent to-orange-400 font-extrabold uppercase">
           Oops!
         </div>
         <div className="-mt-5">
