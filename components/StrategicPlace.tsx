a248aee035cac588cc65d697918118a314dfb031
Ezra Chai <juanzhe2@gmail.com>
2022-12-21 11:57:59 +0800

    fixed security issue

--- components/StrategicPlace.tsx
+++ components/StrategicPlace.tsx
@@ -42,6 +42,7 @@ const StrategicPlace = () => {
         <div className="col-span-2 mt-14 lg:mt-0 overflow-hidden">
           <div className="content-right flex justify-center">
             <iframe
+              title="Google Map for the Cozy & Sweet Homestay Port Dickson's location"
               className=" aspect-square overflow-hidden rounded-lg lg:w-full"
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1993.048515417508!2d101.85464963425447!3d2.474808281170744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cdf77fbf9bcc17%3A0x573234c80a0083d7!2sCozy%20%26%20Sweet%20Homestay!5e0!3m2!1sen!2smy!4v1640871664571!5m2!1sen!2smy"
               allowFullScreen
