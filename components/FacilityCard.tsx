4d418f96cdcac204bdb8e5375630a91a619905f5
ezrachai <juanzhe2@gmail.com>
2023-03-17 00:08:33 +0800

    faciliities

--- components/FacilityCard.tsx
+++ components/FacilityCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const FacilityCard = ({ facility }: any) => {
     return (
-        <div className="mx-4">
+        <div className=" mx-3 md:mx-4">
             <div className="rounded-2xl overflow-hidden">
                 <Image className="aspect-[4/3]" src={facility.image.url} width={facility.image.width} height={facility.image.height} alt={facility.image} />
             </div>
