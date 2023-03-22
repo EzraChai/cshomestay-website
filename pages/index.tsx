a262ed488b80dc68bd81a34d7acfc79a0c7bd845
ezrachai <juanzhe2@gmail.com>
2023-03-22 12:20:39 +0800

    changed height

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home({ data }) {
       </Head>
       <div className="pt-10 pb-10 smooth-scroll big-container hidden h-full bg-white mx-auto">
         <div className="relative wrapper overflow-hidden">
-          <div className="mt-10 md:mt-24 lg:mt-32 absolute lg:rounded-2xl overflow-hidden w-[563px] md:w-[1080px] left-0 right-0 mr-auto ml-auto">
+          <div className="mt-20 md:mt-24 lg:mt-32 absolute lg:rounded-2xl overflow-hidden w-[563px] md:w-[1080px] left-0 right-0 mr-auto ml-auto">
             <video autoPlay loop aria-hidden muted>
               <source src="https://media.graphassets.com/mywVYgpT1C6G9DEMMyZU" type="video/mp4" />
             </video>
@@ -69,7 +69,7 @@ export default function Home({ data }) {
 
           <div className="absolute opacity-70 bg-white w-[563px] md:w-[1080px] h-full left-0 right-0 mr-auto ml-auto"></div>
           <div className="home flex items-center h-[50vh] lg:h-[90vh] w-full flex-col">
-            <div className="mt-[8rem] lg:mt-[16rem]">
+            <div className="mt-[8.2rem] lg:mt-[16rem]">
               <div className="hero-content-line w-full h-100 overflow-hidden">
                 <h1 className="main-text text-center font-extrabold title-font text-[2.8rem] md:text-[5rem] lg:text-[12rem] antialiased">
                   Cozy & Sweet.
