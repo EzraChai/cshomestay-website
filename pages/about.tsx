5b4f304d5a48aba0afbb90d247a1efb8d80ae427
ezrachai <juanzhe2@gmail.com>
2023-04-06 16:53:40 +0800

    changed height of the About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,7 +15,7 @@ export default function Solution() {
       </Head>
       <div className="container hidden bg-zinc-100 mx-auto py-6">
         <div className="wrapper px-2">
-          <div className="home mt-6">
+          <div className="home mt-24">
             <About />
           </div>
         </div>
