ee383a37ee5461e4c0d48666f9401049302a195d
ezrachai <juanzhe2@gmail.com>
2023-04-06 16:41:37 +0800

    added aria-label for whatsapp button

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,37 +1,39 @@
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-
-import { BsWhatsapp } from "react-icons/bs"
-import Link from "next/link"
+import { BsWhatsapp } from "react-icons/bs";
+import Link from "next/link";
 
 const Chat = () => {
-    return (
-        <div className=" fixed right-5 bottom-5 z-[3] bg-green-500 w-16 h-16 md:w-[4.5rem] md:h-[4.5rem] lg:w-20 lg:h-20 flex flex-col items-center justify-center rounded-full">
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <Link
-                            scroll={false}
-                            href={"https://wa.link/z1sa7g"}
-                            target={"_blank"}
-                            rel={"noopener"}>
-                            <div className="relative">
-                                <BsWhatsapp className="text-white w-[2.6rem] h-[2.6rem] md:w-[2.7rem] md:h-[2.7rem]  lg:w-[3.4rem] lg:h-[3.4rem]" />
-                            </div>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <p>Chat with me</p>
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-        </div>
-    )
-}
+  return (
+    <div className=" fixed right-5 bottom-5 z-[3] bg-green-500 w-16 h-16 md:w-[4.5rem] md:h-[4.5rem] lg:w-20 lg:h-20 flex flex-col items-center justify-center rounded-full">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Link
+              role="button"
+              aria-label="Chat using Whatsapp"
+              scroll={false}
+              href={"https://wa.link/z1sa7g"}
+              target={"_blank"}
+              rel={"noopener"}
+            >
+              <div className="relative">
+                <BsWhatsapp className="text-white w-[2.6rem] h-[2.6rem] md:w-[2.7rem] md:h-[2.7rem]  lg:w-[3.4rem] lg:h-[3.4rem]" />
+              </div>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Chat with me</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
