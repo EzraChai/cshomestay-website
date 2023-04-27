87ce7294c5299c4b3340f5fba7f40a5d057fd78a
Ezra Chai <juanzhe2@gmail.com>
2023-04-27 15:37:08 +0800

    changed favicon

--- components/Facilities.tsx
+++ components/Facilities.tsx
@@ -7,7 +7,7 @@ import { A11y, Mousewheel } from "swiper";
 const Facilities = ({ facilities }: any) => {
   return (
     <div className="pt-6 mx-auto mt-24 max-w-7xl lg:mt-0 md:pb-24">
-      <h2 className="text-4xl font-extrabold text-center  lg:text-6xl lg:tracking-tight text-zinc-800">
+      <h2 className="text-4xl font-extrabold text-center lg:text-6xl lg:tracking-tight text-zinc-800">
         Facilities.
       </h2>
       <Swiper
@@ -21,9 +21,10 @@ const Facilities = ({ facilities }: any) => {
             slidesPerView: 3.5,
           },
         }}
+        a11y={{}}
         grabCursor
         modules={[A11y, Mousewheel]}
-        className="mt-6  lg:mt-14"
+        className="mt-6 lg:mt-14"
       >
         {facilities.map((facility) => (
           <SwiperSlide key={facility.id}>
