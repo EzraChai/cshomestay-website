c72f72ec8275f820cf608836441e06a29ae71eed
Ezra Chai <juanzhe2@gmail.com>
2023-06-07 17:06:08 +0800

    added Clarity

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -52,6 +52,13 @@ class MyDocument extends Document {
             strategy={"afterInteractive"}
           />
           {/* <!-- End Cloudflare Web Analytics --> */}
+          <Script type="text/javascript">
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "hg32ok9wtx");
+          </Script>
         </Head>
         <body>
           <Main />
